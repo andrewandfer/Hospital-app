074ae8a9e34cbd816713c2187e7c71eb4ff684cd
Isabel Angulo <isabelf.angulom@uqvirtual.edu.co>
2025-05-14 21:34:12 -0500

    V4 vista de Paciente

---
+++ src/main/java/co/edu/uniquindio/hospital/hospitalapp/hospitalapp/viewController/HistorialMedicoFormViewController.java
@@ -0,0 +1,30 @@
+package co.edu.uniquindio.hospital.hospitalapp.hospitalapp.viewController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HistorialMedicoFormViewController{
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnConsultarHistorialMedico;
+
+    @FXML
+    private TextField btnIdHistorial;
+
+    @FXML
+    void OnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnConsultarHistorialMedico(ActionEvent event) {
+
+    }
+
+}
+
